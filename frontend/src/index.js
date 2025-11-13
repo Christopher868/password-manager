988c9b8a9326e3b4bfb9140559940ae6263270ba
Christopher868 <Christopherhicks868@gmail.com>
2025-11-13 18:57:22 -0500

    Added js file for connecting to supabase and changed colors of page. Also added some content to main.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,20 @@ const collapsingMenu = document.querySelector('#collapsing-menu')
 
 // event listener for opening and closes side menu
 document.addEventListener('click', (e) => {
-    console.log(e.target)
-    // opens menu
-    if(e.target.closest('svg') === menuBtn){
-        collapsingMenu.classList.replace('w-0', 'w-45')
+    // opens and closes menu
+    if(e.target.closest('ul') !== collapsingMenu && e.target.closest('svg') !== menuBtn || e.target.closest('svg') === menuCloseBtn || e.target.closest('svg') === menuBtn && collapsingMenu.classList.contains('w-40')){
+       collapsingMenu.classList.replace('w-40', 'w-0') 
+    } else if(e.target.closest('svg') === menuBtn){
+        collapsingMenu.classList.replace('w-0', 'w-40')
+       
     }    
 
-    // closes menu
-    if(e.target.closest('ul') !== collapsingMenu && e.target.closest('svg') !== menuBtn || e.target.closest('svg') === menuCloseBtn){
-        collapsingMenu.classList.replace('w-45', 'w-0')
+})
+
+
+// Closes side menu after reaching sm breakpoint
+window.addEventListener('resize', () => {
+    if(window.innerWidth >= 640){
+        collapsingMenu.classList.replace('w-40', 'w-0');
     }
 })
