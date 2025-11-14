5136265c5bd4b02736f540440157ce5a63d6663e
Christopher868 <Christopherhicks868@gmail.com>
2025-11-14 01:13:17 -0500

    Added Fontawesome, Added recents accounts, and accounts section that gave them event listeners to open and close them.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,35 @@
 const menuCloseBtn = document.querySelector('#menu-close-btn')
 const menuBtn = document.querySelector('#menu-btn')
 const collapsingMenu = document.querySelector('#collapsing-menu')
+const recentAccounts = document.querySelector('#recent-accounts')
+const recentAccountsBtn = document.querySelector('#recent-accounts-btn')
+const searchAccounts = document.querySelector('#search-accounts')
+const accountsBtn = document.querySelector('#accounts-btn')
+const accountsContainer = document.querySelector('#accounts-container')
 
-// event listener for opening and closes side menu
+const accounts = document.querySelectorAll('.account')
+
+// Event Listener for document
 document.addEventListener('click', (e) => {
-    // opens and closes menu
+    // opens and closes side menu
     if(e.target.closest('ul') !== collapsingMenu && e.target.closest('svg') !== menuBtn || e.target.closest('svg') === menuCloseBtn || e.target.closest('svg') === menuBtn && collapsingMenu.classList.contains('w-40')){
        collapsingMenu.classList.replace('w-40', 'w-0') 
     } else if(e.target.closest('svg') === menuBtn){
         collapsingMenu.classList.replace('w-0', 'w-40')
-       
-    }    
+    }  
+    
+    
+    // Opens and closes recent accounts section
+    if(e.target.closest('h3') === recentAccountsBtn){
+        recentAccounts.classList.toggle('h-0')
+        recentAccounts.classList.toggle('mt-3')
+    } 
+
+    // Opens and closes accounts section
+    if(e.target.closest('h3') === accountsBtn){
+        accountsContainer.classList.toggle('h-0')
+        accountsContainer.classList.toggle('p-5')
+    }
 
 })
 
@@ -21,3 +40,17 @@ window.addEventListener('resize', () => {
         collapsingMenu.classList.replace('w-40', 'w-0');
     }
 })
+
+// Accounts filter
+searchAccounts.addEventListener('input', (e) => {
+    accounts.forEach(account => {
+        if(!account.innerText.toLowerCase().includes(searchAccounts.value.toLowerCase())){
+            account.classList.add('hidden')
+        } else {
+            account.classList.remove('hidden')
+        }
+    })
+})
+
+
+
